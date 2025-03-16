0343e82c087e015824d732ac0f533fd60e70a186
Carlos Bimbela <Carlos Bimbela>
2025-03-16 00:57:05 -0600

    Ej.034

--- 034_Clases.py
+++ 034_Clases.py
@@ -6,10 +6,9 @@ objetos que se crean a partir de ellas pueden tener
 comportamientos y características específicos.
 ¡Son útiles para estructurar el código y reutilizarlo!'''
 
-"""En este capítulo veremos lo que es el método __init__,
-el cuál es un método especial que podemos poner en las
-clases para establecer unos valores iniciales a los
-objetos que se creen a partir de la clase que lo contenga."""
+"""El método __init__, es un método especial que podemos
+poner en las clases para establecer unos valores iniciales
+a los objetos que se creen a partir de la clase que lo contenga."""
 
 '''self es como el this en otros lenguajes de programación.
 Es simplemente una palabra reservada de Python para
@@ -47,4 +46,54 @@ globales y las podemos utilizar tanto dentro como fuera de ellas.'''
 """Una función anidada en Python es una función definida dentro de
 otra función. Nos sirve para encapsular lógica que solo debe usarse
 dentro del contexto de la función externa, ayudando a organizar
-mejor el código y mantenerlo más limpio y seguro."""
+mejor el código y mantenerlo más limpio y seguro.
+Las funciones lambda en Python, también llamadas funciones anónimas,
+son funciones con una sintaxis simplificada que pueden tener solo
+una expresión.
+Son útiles para definir funciones rápidas y temporales, especialmente
+como argumentos de otras funciones, sin necesidad de definirlas."""
+
+# Clase vacía usando pass:
+class ClaseVacia:
+    pass
+
+# Superclase:
+class Persona:
+    def __init__(self, nombre, edad):  # Método __init__ para inicializar atributos
+        self.nombre = nombre
+        self.edad = edad
+
+    def mostrar_info(self):  # Método para mostrar información
+        print("Soy ", self.nombre, " y tengo ", self.edad, " años.")  # Concatenamos con comas
+
+    # Método para incrementar la edad y actualizar una variable global
+    def incrementar_edad(self, age):
+        global edad_global  #Declaramos una variable global
+        self.edad += age
+        edad_global = age  # Actualizamos la variable global
+        print("Ahora tengo ", self.edad, " años.")
+
+# Subclase que hereda de Persona:
+class Empleado(Persona):
+    def __init__(self, nombre, edad, puesto):
+        super().__init__(nombre, edad)  # super() hereda atributos remplaza lo siguiente:
+        # self.nombre = nombre
+        # self.edad = edad
+        self.puesto = puesto
+
+    def mostrar_info(self):  # Método para mostrar información específica del empleado
+        print("Soy ", self.nombre, ", tengo ", self.edad, " años y trabajo como ", self.puesto, ".")
+
+# Creación de objetos:
+persona = Persona("Carlos", 22)
+empleado = Empleado("Bimpolla", 22, "Ingeniero Mecatrónico")
+
+# Uso de los métodos y atributos:
+persona.mostrar_info()
+persona.incrementar_edad(5)
+print("Los años añadidos fueron: ", edad_global) #Usamos la variable global después de ser modificada
+empleado.mostrar_info()
+
+# Ejemplo de clase vacía:
+objeto_vacio = ClaseVacia()
+print("Esto es un objeto de la clase vacía: " + f"{type(objeto_vacio)}")
